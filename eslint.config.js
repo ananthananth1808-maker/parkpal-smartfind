22ca95e303cb4c039add9aa0faceb42b3a5ff303
Abishek Pushparaj <24cs005@psr.edu.in>
2026-04-06 16:35:46 +0530

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from "@eslint/js";
+https://github.com/ananthananth1808-maker/parkpal-smartfind.gitimport js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
